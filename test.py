7efdc88ed8c9ce31c7c3eaf56088b3fe35688b2c
ggopikasri2007 <ggopikasri2007@gmail.com>
2025-10-08 13:58:29 +0530

    create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+-def year(a):
+    if a%4==0:
+        print("leap")
+    elif a%100==0:
+        print("not leap")
+    elif a%400==0:
+        print("leap")
+    else:
+        print("invalid year")
+year(2000)
+year(2025)
